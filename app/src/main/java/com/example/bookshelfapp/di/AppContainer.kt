8c5f6c413c433544aee8a75147d75c503308f433
Lineked <lineked@yandex.ru>
2023-05-28 19:01:34 +0300

    some changes

--- app/src/main/java/com/example/bookshelfapp/di/AppContainer.kt
+++ app/src/main/java/com/example/bookshelfapp/di/AppContainer.kt
@@ -3,6 +3,8 @@ package com.example.bookshelfapp.di
 import com.example.bookshelfapp.data.BooksListRepository
 import com.example.bookshelfapp.data.NetworkBooksListRepository
 import com.example.bookshelfapp.network.BookshelfApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,9 +13,16 @@ interface AppContainer {
 }
 
 class DefaultAppContainer(): AppContainer {
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        this.level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
     private val BASE_URL = "https://www.googleapis.com/books/v1/"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
