d6bf97f5175238c01f2c8de2816373629e49210f
Lineked <lineked@yandex.ru>
2023-10-24 09:15:32 +0300

    Удалён IconButton

--- app/src/main/java/com/example/bookshelfapp/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/screens/search/SearchScreen.kt
@@ -11,15 +11,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -113,7 +111,7 @@ fun BooksListScreen(
                 title = it.title,
                 author = it.author,
                 imageUrl = it.imageUrl,
-                onBookItemClick = {onBookItemClick(it.id)}
+                onBookItemClick = {onBookItemClick(it.id)},
             )
         }
     }
@@ -153,9 +151,6 @@ fun BookItem(
                     overflow = TextOverflow.Ellipsis,
                     maxLines = 1
                 )
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.FavoriteBorder, contentDescription = "")
-                }
             }
         }
     }
