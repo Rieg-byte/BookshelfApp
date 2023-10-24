45e342abf285d122436582938533a20a782a3fe5
Lineked <lineked@yandex.ru>
2023-10-24 15:37:55 +0300

    Добавлено отображение списка избранных книг

--- app/src/main/java/com/example/bookshelfapp/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/screens/favorites/FavoritesScreen.kt
@@ -1,15 +1,36 @@
 package com.example.bookshelfapp.ui.screens.favorites
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bookshelfapp.ui.screens.search.BookItem
 
 @Composable
-fun FavoritesScreen(){
-    Column(modifier = Modifier.fillMaxSize().background(Color.Gray)) {
-
+fun FavoritesScreen(
+    favoritesViewModel: FavoritesViewModel = viewModel(factory = FavoritesViewModel.Factory)
+){
+    val favoritesUiState by favoritesViewModel.favoritesUiState.collectAsState()
+    Column(modifier = Modifier
+        .fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            items(favoritesUiState.listOfFavorites) {
+                BookItem(title = it.title, author = it.author, imageUrl = it.imageUrl, onBookItemClick = { /*TODO*/ })
+            }
+        }
     }
 }
