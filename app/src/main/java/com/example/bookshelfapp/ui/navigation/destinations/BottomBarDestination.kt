9f87fbdc9df2aa74cd839d3bcb74723dfdc85e23
Lineked <lineked@yandex.ru>
2023-10-24 15:59:41 +0300

    Удалён enum-класс BOOSK

--- app/src/main/java/com/example/bookshelfapp/ui/navigation/destinations/BottomBarDestination.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/navigation/destinations/BottomBarDestination.kt
@@ -7,7 +7,6 @@ import com.example.bookshelfapp.R
 import com.example.bookshelfapp.ui.icons.BookshelfIcons
 
 enum class BottomBarDestination(@StringRes val label: Int, val icon: ImageVector){
-    BOOKS(label = R.string.books, icon = BookshelfIcons.LibraryBooks),
     FAVORITES(label = R.string.favorites, icon = BookshelfIcons.Bookmarks),
     SEARCH(label = R.string.search, icon = BookshelfIcons.Search)
 }
