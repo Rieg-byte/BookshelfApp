af44adce42ee23313d772dd7ae203c45f1e41143
Lineked <lineked@yandex.ru>
2023-10-24 09:17:25 +0300

    Добавлена функция convertBookInfoToFavoriteEntity()

--- app/src/main/java/com/example/bookshelfapp/data/remote/model/BookInfo.kt
+++ app/src/main/java/com/example/bookshelfapp/data/remote/model/BookInfo.kt
@@ -1,9 +1,19 @@
 package com.example.bookshelfapp.data.remote.model
 
+import com.example.bookshelfapp.data.local.entities.Favorite
+
 data class BookInfo(
+    val id: String,
     val title: String,
     val description: String,
     val author: String,
     val imageUrl: String,
     val previewLink: String
-)
+) {
+    fun convertBookInfoToFavoriteEntity(): Favorite = Favorite(
+        id = id,
+        author = author,
+        title = title,
+        imageUrl = imageUrl
+    )
+}
