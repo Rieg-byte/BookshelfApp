d1b589545057d833cbf442fdc709a301c4e886b6
Lineked <lineked@yandex.ru>
2023-10-25 20:30:48 +0300

    Добавлен переход в DetailScreen с FavoritesScreen

--- app/src/main/java/com/example/bookshelfapp/ui/navigation/BookshelfNavHost.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/navigation/BookshelfNavHost.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.example.bookshelfapp.ui.navigation.destinations.BottomBarDestination
+import com.example.bookshelfapp.ui.navigation.destinations.FavoriteDestination
 import com.example.bookshelfapp.ui.navigation.destinations.SearchDestination
 import com.example.bookshelfapp.ui.screens.detail.DetailScreen
 import com.example.bookshelfapp.ui.screens.favorites.FavoritesScreen
@@ -21,9 +22,7 @@ fun BookshelfNavHost(
     navController: NavHostController
 ) {
     NavHost(navController = navController, startDestination = BottomBarDestination.FAVORITES.name) {
-        composable(BottomBarDestination.FAVORITES.name) {
-            FavoritesScreen()
-        }
+        favoriteGraph(navController = navController)
         searchGraph(navController = navController)
     }
 }
@@ -47,6 +46,29 @@ fun NavGraphBuilder.searchGraph(navController: NavController) {
         }
         composable(
             route = "${SearchDestination.DETAIL_SCREEN.name}/{bookId}",
+            arguments = listOf(navArgument("bookId") { type = NavType.StringType })
+        ) {
+            DetailScreen(navigateUp = {navController.navigateUp()})
+        }
+    }
+}
+
+fun NavGraphBuilder.favoriteGraph(navController: NavController) {
+    navigation(
+        startDestination = FavoriteDestination.FAVORITE_SCREEN.name,
+        route = BottomBarDestination.FAVORITES.name
+    ) {
+        composable(FavoriteDestination.FAVORITE_SCREEN.name) {
+            FavoritesScreen(
+                navigateToDetailScreen = { bookId ->
+                    navController.navigate("${FavoriteDestination.DETAIL_SCREEN.name}/$bookId") {
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+        composable(
+            route = "${FavoriteDestination.DETAIL_SCREEN.name}/{bookId}",
             arguments = listOf(navArgument("bookId") { type = NavType.StringType })
         ) {
             DetailScreen(navigateUp = {navController.navigateUp()})
