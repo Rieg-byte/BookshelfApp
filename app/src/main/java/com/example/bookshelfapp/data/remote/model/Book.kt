3f6f065a247854bec35895cbcb03063a95e4ac8b
Lineked <lineked@yandex.ru>
2023-10-26 14:45:42 +0300

    Добавлены convertToBookInfo() и convertToItem()

--- app/src/main/java/com/example/bookshelfapp/data/remote/model/Book.kt
+++ app/src/main/java/com/example/bookshelfapp/data/remote/model/Book.kt
@@ -3,5 +3,21 @@ package com.example.bookshelfapp.data.remote.model
 data class Book(
     val id: String,
     val volumeInfo: VolumeInfo
-)
+) {
+    fun convertToBookInfo() =
+        BookInfo(
+        id = id,
+        title = volumeInfo.title,
+        description = volumeInfo.description ?: "",
+        author = volumeInfo.authors?.joinToString() ?: "",
+        imageUrl = volumeInfo.imageLinks?.smallThumbnail ?: "",
+        previewLink = volumeInfo.previewLink ?: ""
+    )
+    fun convertToItem() = Item(
+        id = id,
+        author = volumeInfo.authors?.joinToString() ?: "",
+        title = volumeInfo.title,
+        imageUrl = volumeInfo.imageLinks?.smallThumbnail ?: "",
+    )
+}
 
