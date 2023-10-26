8840f0b601181040c5084041eb85fc099c523d0a
Lineked <lineked@yandex.ru>
2023-10-26 14:53:56 +0300

    Преобразование в Item вынесено в отдельную функцию

--- app/src/main/java/com/example/bookshelfapp/data/BooksRepositoryImpl.kt
+++ app/src/main/java/com/example/bookshelfapp/data/BooksRepositoryImpl.kt
@@ -7,27 +7,7 @@ import com.example.bookshelfapp.data.remote.model.Item
 class BooksRepositoryImpl(
     private val booksRemoteDataSource: BooksRemoteDataSource,
 ): BooksRepository {
-    override suspend fun getBooksList(query: String): List<Item> {
-        val result = booksRemoteDataSource.getBookList(query).items
-        return result?.map {
-            Item(
-                id = it.id,
-                title = it.volumeInfo.title,
-                imageUrl = it.volumeInfo.imageLinks?.smallThumbnail ?: "",
-                author = it.volumeInfo.authors?.joinToString() ?: ""
-            )
-        } ?: emptyList()
-    }
+    override suspend fun getBooksList(query: String): List<Item> = booksRemoteDataSource.getBookList(query).convertToItemList()
 
-    override suspend fun getBookInfo(path: String): BookInfo {
-        val result = booksRemoteDataSource.getBookInfo(path)
-        return BookInfo(
-            id = result.id,
-            title = result.volumeInfo.title,
-            description = result.volumeInfo.description ?: "",
-            author = result.volumeInfo.authors?.joinToString() ?: "",
-            imageUrl = result.volumeInfo.imageLinks?.smallThumbnail ?: "",
-            previewLink = result.volumeInfo.previewLink ?: ""
-        )
-    }
+    override suspend fun getBookInfo(path: String): BookInfo = booksRemoteDataSource.getBookInfo(path).convertToBookInfo()
 }
