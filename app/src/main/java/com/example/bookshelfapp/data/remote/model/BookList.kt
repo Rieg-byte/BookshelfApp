39f21ab106c5a6c57efce490b13fea074ad43885
Lineked <lineked@yandex.ru>
2023-10-26 14:52:33 +0300

    Добавлен convertToItemList()

--- app/src/main/java/com/example/bookshelfapp/data/remote/model/BookList.kt
+++ app/src/main/java/com/example/bookshelfapp/data/remote/model/BookList.kt
@@ -4,6 +4,8 @@ package com.example.bookshelfapp.data.remote.model
 data class BookList(
     val totalItems: String,
     val items: List<Book>?
-)
+) {
+    fun convertToItemList(): List<Item> = items?.map { it.convertToItem() } ?: emptyList()
+}
 
 
