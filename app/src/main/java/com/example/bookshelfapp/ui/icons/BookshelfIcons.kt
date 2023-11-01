2b9e33dfdeb4f27acb807f0e803bb2e6d4d0baea
Lineked <lineked@yandex.ru>
2023-11-01 21:41:02 +0300

    Добавлены новые иконки

--- app/src/main/java/com/example/bookshelfapp/ui/icons/BookshelfIcons.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/icons/BookshelfIcons.kt
@@ -4,13 +4,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Bookmarks
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.LibraryBooks
+import androidx.compose.material.icons.rounded.DeleteOutline
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.Search
 
 object BookshelfIcons {
     val Search = Icons.Rounded.Search
     val Bookmarks = Icons.Rounded.Bookmarks
     val ArrowBack = Icons.Rounded.ArrowBack
-    val LibraryBooks = Icons.Rounded.LibraryBooks
+    val Favorite = Icons.Rounded.Favorite
     val Close = Icons.Rounded.Close
+    val FavoriteBorder = Icons.Rounded.FavoriteBorder
+    val DeleteOutline = Icons.Rounded.DeleteOutline
 }
